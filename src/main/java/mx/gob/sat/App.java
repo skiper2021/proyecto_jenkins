63e34d3a414315c9f39d83b0b018fb154e371a6e
jorge sanchez <jorge.sanchez@sat.gob.mx>
2021-12-28 15:56:50 -0600

    Nuevo cambio 28/12/2021

--- src/main/java/mx/gob/sat/App.java
+++ src/main/java/mx/gob/sat/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "La conecion a Github se ha hecho");
     }
 }
